29f61b8764f3d30a70fb771d529dbf208e6f8010
ziad el-khashab <93553215+zeyadnagi2@users.noreply.github.com>
2023-05-04 22:53:52 +0300

    Add files via upload

---
+++ computer-graphics/Graphics/GraphicsForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Graphics
+{
+    partial class GraphicsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.programTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(575, 357);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.simpleOpenGlControl1_KeyPress);
+            // 
+            // programTimer
+            // 
+            this.programTimer.Interval = 5;
+            this.programTimer.Tick += new System.EventHandler(this.programTimer_Tick);
+            // 
+            // GraphicsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 357);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "GraphicsForm";
+            this.Text = "Graphics";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GraphicsForm_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer programTimer;
+    }
+}
+
---
+++ computer-graphics/Graphics/GraphicsForm.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Forms;
+using System.Threading;
+namespace Graphics
+{
+    public partial class GraphicsForm : Form
+    {
+        Renderer renderer = new Renderer();
+        
+        public GraphicsForm()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+            initialize();
+            programTimer.Enabled = true;
+        }
+        void initialize()
+        {
+            renderer.Initialize();   
+        }
+        
+        private void GraphicsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            renderer.CleanUp();
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            renderer.Update();
+            renderer.Draw();
+        }
+
+        private void programTimer_Tick(object sender, System.EventArgs e)
+        {
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void simpleOpenGlControl1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            float step = 0.5f;
+            switch (e.KeyChar)
+            {
+                
+            }
+            simpleOpenGlControl1.Refresh();
+        }
+
+    }
+}
